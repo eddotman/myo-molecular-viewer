e5448cba61b10a8c921d097910ec2610c7122ecc
Edward Kim <eddotman@gmail.com>
2015-01-24 14:05:52 -0500

    sorta works with gestures

--- myomolviewer.lua
+++ myomolviewer.lua
@@ -4,16 +4,32 @@ scriptDetailsUrl = "" -- We don't have this until it's submitted to the Myo Mark
 
 function onPoseEdge(pose, edge)
     myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+
+    if (pose == "fist" and edge == "on") then
+        myo.mouse("left", "down")
+    else
+        myo.mouse("left", "up")
+    end
 end
 
 function onPeriodic()
+    -- if (myo.mouseControlEnabled()) then
+    --     myo.mouse("left", "down")
+    -- end
 end
 
 function onForegroundWindowChange(app, title)
     myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
-    myo.mouse("left", "down")
-    myo.controlMouse(true)
-    return (app == "net.java.openjdk.cmd")  --ID for Jmol
+    if (not myo.mouseControlEnabled()) then
+        myo.controlMouse(true)
+    end
+    if (app == "net.java.openjdk.cmd") then  --ID for Jmol
+        return true
+    else
+        myo.controlMouse(false)
+        myo.mouse("left", "up")
+        return false
+    end
 end
 
 function activeAppName()
